40dd0607340370b3790566b1eddf273230659103
CmaillaKurup <cami670v@zbc.dk>
2020-08-28 12:22:13 +0200

    First commit

---
+++ WashingMachine/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WashingMachine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
